17dc55eae495018f60f51b6e763de9ed90d6fc92
tblais1224 <thomasblais.student@careerdevs.com>
2019-05-01 15:40:05 -0400

    finished 15 and 16

--- myapp/src/pokemon.js
+++ myapp/src/pokemon.js
@@ -1,25 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 
-class Pokemon extends Component {
-    render(){
-        const { pokemon } = this.props;
-        const pokemonList = pokemon.map(pokemon => {
-        
-        return(
-            <div className="pokemon" key={pokemon.id}>
-                <div>Name: {pokemon.name}</div>
-                <div>lvl: {pokemon.lvl}</div>
-                <div>move: {pokemon.move}</div>
-            </div>
-        )
-        })
-        return(
-            <div className="pokemon-list">
-                {pokemonList}
-            </div>
-        )
-    }
+const Pokemon = ({ pokemon }) => {
+
+    // const pokemonList = pokemon.map(pokemon => {
+    //     if(pokemon.lvl >= 24){
+
+    //     return (
+    //         <div className="pokemon" key={pokemon.id}>
+    //             <div>Name: {pokemon.name}</div>
+    //             <div>lvl: {pokemon.lvl}</div>
+    //             <div>move: {pokemon.move}</div>
+    //         </div>
+    //     )} else {
+    //         return (<div>no eligible pokemon</div>)
+    //     }
+    // })
+
+    return (
+        <div className="pokemon-list">
+            {
+                pokemon.map(pokemon => {
+                    //ternary operator
+                    return pokemon.lvl > 28 ? (
+                        <div className="pokemon" key={pokemon.id}>
+                            <div>Name: {pokemon.name}</div>
+                            <div>lvl: {pokemon.lvl}</div>
+                            <div>move: {pokemon.move}</div>
+                        </div>
+                    ) : null;
+                })
+            }
+        </div>
+    )
 }
+
 
 export default Pokemon
